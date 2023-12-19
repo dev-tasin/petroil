b10a2b58b6164e54a69f62b42e0344b8f974261e
dev-tasin <ahmedtasin94@gmail.com>
2023-12-20 01:36:31 +0600

    header and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        headerbg: '#282828',
+        primary: '#F40404',
+      },
+      fontFamily: {
+        'poppins': "'Poppins', sans-serif",
+      },
+      spacing: {
+        'container': '1144px',
+      }
+    }
+  }
+}
